c0b83eaf6f75ab72eeb20980b2f22e078af00ac0
Victor Gallegos <316160456@ciencias.unam.mx>
2022-12-21 00:24:16 -0600

    Agrega Comunicacion de Datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Victor';
+  age = 23;
+  img = 'https://source.unsplash.com/random/200x200';
+  btnDisabled = true;
+  progress = 50;
+  person = {
+    name: 'Hugo',
+    age: 23,
+    profession: 'Developer',
+    state: 'active',
+    profileimg: 'https://source.unsplash.com/random/200x200',
+  };
+
+  toggleButtton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  incrementarEdad() {
+    this.person.age++;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
